d17fc4714c005307c583338a90dda00662c0ef4a
Geethika Priya <75033631+geethika02@users.noreply.github.com>
2021-04-10 19:45:09 +0530

    Create pattern2.c

---
+++ pattern2.c
@@ -0,0 +1,23 @@
+/*
+5
+1
+12
+123
+1234
+12345
+*/
+
+#include <stdio.h>
+
+int main(){
+	int n,i,j;
+	scanf("%d", &n);        
+	for(i=0;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
